734a79de43eb62f533218d59e541ff05147b5b8c
prkrisha <prkrisha@sheridancollege.ca>
2024-09-20 19:10:23 -0400

    Update Card.java

--- src/card/Card.java
+++ src/card/Card.java
@@ -11,7 +11,8 @@ package card;
  * There are 52 cards in a deck, no jokers.
  * This code is to be used in ICE1. When you create your own branch,
  * add your name as a modifier.
- * @author srinivsi
+ * Modifier: KRISHAL
+ * Student Number: 991711226
  */
 public class Card {
 
